a0ffd0638b83ce7963d7c82fde4bf193c86a74aa
FA-lex <a.good.lex@gmail.com>
2022-07-03 00:32:44 +0800

    complit

--- interface.py
+++ interface.py
@@ -1,5 +1,3 @@
-from unittest import result
-
 
 def main_menu():
     print('-'*40)
@@ -9,13 +7,15 @@ def main_menu():
         result_request = int(input())
     except:
         return exit()
-    return result_request
+    return result_request if 0 < result_request <= 3 else exit()
 
 
 def get_find():
-    param = input('По какому параметру будем искать? имя, фамилия или номер\n_')
-    value = input('Что будем искать? \n_')
-    return (value, param)
+    try:
+        param = int(input('По какому параметру будем искать? 1-имя, 2-фамилия или 3-номер\nлюбая другая - в главное меню\n_'))
+    except:
+        return False
+    return (input('Что будем искать? \n_'), param) if 0 < param <= 3 else False
 
 def action_contact():
     print('1 - изменить контакт\n2 - удалить контакт\n3 - поделиться контактом\n0 - выйти в главное меню\nлюбая другая - выход из программы')
@@ -24,16 +24,71 @@ def action_contact():
         result_request = int(input())
     except:
         return exit()
-    return result_request if 0 <= result_request <= 3 else exit()
+    if result_request == 0: return False
+    return result_request if 0 < result_request <= 3 else exit()
 
 def select_contact():
     return int(input('выберите контакт:'))
 
 def print_selected_contact(selected_contact):
-    print(f'выбран контакт: {selected_contact}')
+    print(f'выбран контакт: {selected_contact[1]}, {selected_contact[2]}, {selected_contact[3]}, {selected_contact[4]}\n'+'-'*40)
+
+def print_report_delete_contact(contact):
+    print(f'контакт: {contact[1]}, {contact[2]}, {contact[3]}, {contact[4]}: удален\n'+'-'*40)
+
+def print_report_change_contact(contact):
+    print(f'контакт изменен на: {contact[1]}, {contact[2]}, {contact[3]}, {contact[4]}\n'+'-'*40)
+
+def print_report_new_contact(contact):
+    print(f'создан контакт: {contact[1]}, {contact[2]}, {contact[3]}, {contact[4]}\n'+'-'*40)
+
+def print_export(contact, file):
+    print(f'контакт: {contact[0]}, {contact[1]}, {contact[2]} передан в файл: {file} \n'+'-'*40)
+
+def get_change():
+    print('выберите что надо изменить:\n1-имя\n2-фамилию\n3-телефон\n4-коментарий\n0-отмена')
+    flag = False
+    while not flag:
+        try:
+            area = int(input())
+        except:
+            print('введите цифру от 1 до 4')
+        if 0 < area <= 4: flag = True
+    changed_area =input('введите новые данные: ')
+    return (area, changed_area)
+
+
+def get_new_contact():
+    print('-'*40)
+    first_name = input('введите имя:')
+    last_name = input('введите фамилию:')
+    phone = input('введите телефон:')
+    cometary = input('введите коментарий:')
+    print('-'*40)
+    return ['', first_name, last_name, phone, cometary]
+
+
+def get_type_file():
+    print('в каком из форматов будем экспортировать контакт? 1-.csv 2-.txt, что-нибудь другое-отмена')
+    try:
+        type = int(input())
+        return type if 0 < type <= 2 else type/0
+    except:
+        print('введите цифру 1 или 2')
+
+
+def create_dubl():
+    try:
+        dubl = int(input('Такой контакт уже есть. Хотите сделать дубликат? 1-да, что-нибудь другое-отмена'))
+        return True if dubl == 1 else dubl/0
+    except:
+        print('Вы отменили создание контакта')
+        return False
     
+    
+   
 
 if __name__ == '__main__':
     print(main_menu())
-    print(what_find())
+    print(get_find())
     print(action_contact())
